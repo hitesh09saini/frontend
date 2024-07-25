ebe0f96639a69cb5d96a3d3f03591f0a612e9272
Hitesh saini <128598996+hitesh09saini@users.noreply.github.com>
2024-07-25 23:32:45 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ const roboto = Protest_Strike({
 
 export const metadata = {
   title: "Hitesh Folio",
-  description: "Hello, I'm Hitesh Saini, currently residing in Mahwa, Dausa, Rajasthan. After successfully completing my studies at Arya College, Kukas, Jaipur, where I obtained a B.Tech degree in Electrical Engineering, I've dedicated myself to the field of full-stack development at DEVHUB Tech. Alongside my professional endeavors, I actively immerse myself in freelancing projects during my leisure hours, continuously expanding my skill set and contributing to diverse projects.",
+  description: "Hello, I'm Hitesh Saini, Software Engineer, 21 year old, currently residing in Mahwa, Dausa, Rajasthan. After successfully completing my studies at Arya College, Kukas, Jaipur, where I obtained a B.Tech degree in Electrical Engineering, I've dedicated myself to the field of full-stack development at DEVHUB Tech. Alongside my professional endeavors, I actively immerse myself in freelancing projects during my leisure hours, continuously expanding my skill set and contributing to diverse projects.",
   icons: {
     icon: ['/favicon.ico?v=4'],
     apple: ['/apple-touch-icon.png?v=4'],
