946e9715666c8c85bb066f832e6adffc3eb7b5c3
Hitesh saini <128598996+hitesh09saini@users.noreply.github.com>
2024-10-26 22:44:28 +0530

    Update Experience.js

--- src/app/component/Experience.js
+++ src/app/component/Experience.js
@@ -4,7 +4,8 @@ const Experience = () => {
   return (
     <div className='min-h-[300px] mb-[100px] bg-[#EEE9EB] pt-8 relative p-6 flex flex-col gap-8'>
      <h1  className='text-4xl'>Work !</h1>
-       <Work company="DevHub Tech." role="Software Engineer" date="Mar 2024 - Present" />
+       <Work company="AbyM Tech." role="Software Engineer" date="Oct 2024 - Present" />
+       <Work company="DevHub Tech." role="Software Engineer" date="Mar 2024 - Sept 2024" />
     </div>
   )
 }
