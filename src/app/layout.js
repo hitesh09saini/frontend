a1e14b9ab658bf0c5119c3f5ee03e8601b02ebeb
Hitesh saini <128598996+hitesh09saini@users.noreply.github.com>
2025-09-03 01:00:40 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Protest_Strike } from "next/font/google";
 import Head from 'next/head';
-
 import "./globals.css";
-import Script from "next/script";
+import ServiceWorkerRegister from "../../ServiceWorkerRegister";
+
 const roboto = Protest_Strike({
   subsets: ["latin"],
   weight: ['400']
@@ -10,15 +10,14 @@ const roboto = Protest_Strike({
 
 export const metadata = {
   title: "Hitesh Folio",
-  description: "Hello, I'm Hitesh Saini, Software Engineer, 21 year old, currently residing in Mahwa, Dausa, Rajasthan. After successfully completing my studies at Arya College, Kukas, Jaipur, where I obtained a B.Tech degree in Electrical Engineering, I've dedicated myself to the field of full-stack development at DEVHUB Tech. Alongside my professional endeavors, I actively immerse myself in freelancing projects during my leisure hours, continuously expanding my skill set and contributing to diverse projects.",
+  description: "Portfolio of Hitesh Saini",
+  manifest: '/site.webmanifest',
   icons: {
     icon: ['/favicon.ico?v=4'],
     apple: ['/apple-touch-icon.png?v=4'],
     shortcut: ['/apple-touch-icon.png'],
-  },
-  manifest: '/site.webmanifest'
+  }
 };
-
 
 export default function RootLayout({ children }) {
   return (
@@ -27,7 +26,10 @@ export default function RootLayout({ children }) {
         <title>Hitesh Saini</title>
         <meta name="description" content="Hitesh Saini का पोर्टफोलियो." />
       </Head>
-      <body suppressHydrationWarning={true} className={`${roboto.className} overflow-x-hidden`} >{children}</body>
+      <body suppressHydrationWarning={true} className={`${roboto.className} overflow-x-hidden`}>
+        {children}
+        <ServiceWorkerRegister />
+      </body>
     </html>
   );
 }
